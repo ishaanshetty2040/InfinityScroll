0448e10532846483702c12b3518d381325f7393b
Ishaan <ishaanshetty12@gmail.com>
2022-05-25 09:44:08 +0530

    files modified

--- script.js
+++ script.js
@@ -8,19 +8,37 @@ const count = 10;
 const apiKey = '35-SzVm2ILRdQTwyJMkKfZdVsOybDdj12b3JjwbitCU';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+
+}
+
+
 //Create elements for links & photos, Add to DOM
 function displayPhotos() {
     //Run function for each object in photosArray
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html)
-        item.setAttribute('target', '_blank');
+        /* item.setAttribute('href', photo.links.html)
+        item.setAttribute('target', '_blank'); */
         //Create <img> for photo
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
+        /* img.setAttribute('src', photo.urls.regular);
         img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        img.setAttribute('title', photo.alt_description); */
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
         //Put <img. inside <a> then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -34,6 +52,7 @@ async function getPhotos() {
         const response = await fetch(apiUrl);
         photosArray = await response.json();
         displayPhotos();
+        /* console.log(photosArray); */
     } catch (error) {
         //Catch error here
     }
